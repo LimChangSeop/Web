3c374022fc563844c86857190f268db087f0979e
dlack <dlack@DESKTOP-G9U4LB6>
2022-10-22 20:00:47 +0900

    20221022

---
+++ JavaStudy 20221022 Nemnem/src/N01_출력/MyInfo.java
@@ -0,0 +1,25 @@
+package N01_출력;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		char name1 = '임';
+		char name2 = '창';
+		char name3 = '섭';
+		
+		// char은 문자, string은 문자열
+		// 문자는 '' , 문자열은 "" 사용
+		String name = "임창섭";
+		String address = "사랑시 고백구 행복동";
+		int age = 25;
+		
+		System.out.print("이름 : ");
+		System.out.println(name);
+		System.out.print("나이 : ");
+		System.out.println(age);
+		System.out.print("주소 : ");
+		System.out.println(address);
+
+	}
+
+}
