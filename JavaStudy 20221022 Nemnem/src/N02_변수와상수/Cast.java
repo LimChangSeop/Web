2cc3ad4b9b1eaa037632f2ba8168ddc5897f0b08
dlack <dlack@DESKTOP-G9U4LB6>
2022-10-22 20:17:13 +0900

    cast update

--- JavaStudy 20221022 Nemnem/src/N02_변수와상수/Cast.java
+++ JavaStudy 20221022 Nemnem/src/N02_변수와상수/Cast.java
@@ -3,8 +3,23 @@ package N02_변수와상수;
 public class Cast {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// 업캐스팅 할 때 순서 문자 -> 정수 -> 실수
+		char a = 'A';
+		int b = a;
+		double c = b;
+		
+		System.out.println(b);
+		System.out.println(c);
+		
+		// 다운 캐스팅 할때는 명시를 해줘야 한다.
+		int d = (int) c;
+		char e = (char) d;
+		
+		System.out.println(d);
+		System.out.println(e);
+		
+		// char은 바로 double로 넣지 못함. 무조건 int로만 가능.
 	}
-
+ 
 }
